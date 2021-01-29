3c29af0928a759bf1c75c3a9ea5daa92f969cdf5
Azamat9904 <azamatnamialy@gmail.com>
2021-01-29 18:08:00 +0600

    feature(): Main page

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const bannerSwiper = new Swiper('.banner-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
+
+
+const categoriesSwiper = new Swiper('.categories-container', {
+    slidesPerView: 5,
+    spaceBetween: 95,
+     navigation: {
+        nextEl: '.swiper-button-next',
+      },
+})
+
+$('#play-video').on('click', function(){
+    $('.video-content .embed-responsive-item').attr('src', 'https://www.youtube.com/embed/zpOULjyy-n8?rel=0').attr('allow', 'autoplay');
+    $('.video-content').removeClass('video-content-bg');
+    $('.video-content button').hide();
+});
